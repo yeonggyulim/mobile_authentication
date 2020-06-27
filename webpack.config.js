7a011ae85b67b77fe1e860c44a2cae0ed589a7d1
yeonggyulim <lyg940128@gmail.com>
2020-06-27 10:16:17 +0900

    babel, webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+
+	entry: ['@babel/polyfill', './src/index.js'],
+
+	output: {
+		path: path.resolve(__dirname, 'public/js'),
+		filename: '[name].bundle.js',
+	},
+
+	devtool: 'inline-source-map',
+
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Mobile Authentication',
+			filename: './index.html',
+		}),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: [path.resolve(__dirname, 'src/')],
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-class-properties'],
+					},
+				},
+			},
+		],
+	},
+};
